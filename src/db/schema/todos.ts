f472efc34759f4d734ec7a470a89278873233392
Shivam Karn <shivamkarn767@gmail.com>
2026-05-19 19:09:34 +0545

    Completed todos schemas

--- src/db/schema/todos.ts
+++ src/db/schema/todos.ts
@@ -1,1 +1,38 @@
-export {};
+import { sql } from "drizzle-orm";
+import { sqliteTable, text, integer, index } from "drizzle-orm/sqlite-core";
+import { user } from "./users";
+
+const timestamps = {
+  createdAt: integer("created_at", { mode: "timestamp_ms" })
+    .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
+    .notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp_ms" })
+    .default(sql`(cast(unixepoch('subsecond') * 1000 as integer))`)
+    .$onUpdate(() => new Date())
+    .notNull(),
+};
+
+export const todos = sqliteTable(
+  "todos",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    userId: text("user_id")
+      .notNull()
+      .references(() => user.id, { onDelete: "cascade" }),
+    title: text("title").notNull(),
+    description: text("description"),
+    completed: integer("completed", { mode: "boolean" }).default(false),
+    dueDate: text("due_date"),
+    completedAt: text("completed_at"),
+    priority: text("priority", { enum: ["low", "medium", "high"] }).default(
+      "medium",
+    ),
+    deletedAt: integer("deleted_at", {
+      mode: "timestamp_ms",
+    }),
+    ...timestamps,
+  },
+  (table) => [index("index_on_todos_userId").on(table.userId)],
+);
