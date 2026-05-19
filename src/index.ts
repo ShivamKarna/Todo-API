64b04af3886c493ccc3cca98ec7b283722fdc20f
Shivam Karn <shivamkarn767@gmail.com>
2026-05-19 19:09:08 +0545

    Put todo router in main index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
+import { todoRouter } from "./modules/todos/todos.routes";
 
 const app = new Hono();
+
+app.route("/api/todo", todoRouter);
 
 app.get("/", (c) => {
   return c.text("Hello this is todo backend");
