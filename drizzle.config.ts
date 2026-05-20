18da7387013a38b00aa10b8b60ebfc646679f5df
Shivam Karn <shivamkarn767@gmail.com>
2026-05-20 08:51:26 +0545

    Migrations 20 may 8:51am

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/db/schema.ts",
-  out: "./drizzle/migrations",
+  schema: "./src/db/schema",
+  out: "./migrations",
   dialect: "sqlite",
 } satisfies Config;
